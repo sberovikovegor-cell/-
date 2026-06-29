317bfadf4651e98f4d8cb5c4511114c68712c793
sberovikovegor-cell <sberovikovegor@gmail.com>
2026-06-29 16:49:29 +0500

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const DEVICE_ID_KEY = "family-counter-device-id";
 const SESSION_ACTIVE_KEY = "family-counter-session-active";
 const STARTUP_PUSH_DONE_KEY = "family-counter-startup-push-done";
 const CLOUD_CONFIRM_FP_KEY = "family-counter-cloud-confirm-fp";
-const APP_BUILD = "115";
+const APP_BUILD = "116";
 
 let coldAppLaunch = false;
 let cloudConfirmTimer = null;
